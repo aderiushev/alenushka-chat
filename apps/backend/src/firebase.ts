e9d782575e76d7426002d85286323ade6b0604c8
Aleksei <derushev.aleksei@gmail.com>
2025-04-16 12:54:04 +0200

    temp

--- apps/backend/src/firebase.ts
+++ apps/backend/src/firebase.ts
@@ -1,6 +1,5 @@
 import * as admin from 'firebase-admin';
-import { join } from 'path';
-import * as serviceAccount from './alenushka-chat-eaa6a-firebase-adminsdk-fbsvc-d861ab582c.json';
+import * as serviceAccount from './alenushka-chat-eaa6a-firebase-adminsdk-fbsvc-28c16b5a00.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
