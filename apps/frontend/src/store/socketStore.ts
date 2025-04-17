68d7eeecf717f203c60603587087d4d14942a31f
Aleksei <derushev.aleksei@gmail.com>
2025-04-17 15:02:56 +0200

    temp

--- apps/frontend/src/store/socketStore.ts
+++ apps/frontend/src/store/socketStore.ts
@@ -20,15 +20,12 @@ export const useSocketStore = create<SocketState>((set, get) => ({
       existingSocket.disconnect();
     }
 
-    const socket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:4001' : `${window.location.origin}`, {
-      extraHeaders: {
-        Authorization: token ? `Bearer ${token}` : '',
-      },
-      path: '/api/socket.io',
-      transports: ['websocket'],
+    const socket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:4001' : `${window.location.protocol}//${window.location.hostname}`, {
       query: {
         token,
       },
+      path: '/api/socket.io',
+      transports: ['websocket'],
     });
 
     socket.on('initial-messages', (initialMessages: Message[]) => {
